c52d9f748bd314a4e63491100d46f763d92eb865
MdAbuessa <abuessaflutterdev@gmail.com>
2026-06-27 17:36:04 +0600

    update language for eng rewuired and logo change

--- lib/feature/home/data/rx_wisdom_generate/rx.dart
+++ lib/feature/home/data/rx_wisdom_generate/rx.dart
@@ -34,11 +34,15 @@ final class WisdomGenerateRx extends RxResponseInt<WisdomGenerateModel> {
     if (error is DioException) {
       if (error.response?.statusCode == 400) {
         ToastUtil.showShortToast(
-          error.response?.data["error"] ?? error.message,
+          error.response?.data["message"] ?? error.message,
+        );
+      } else if (error.response?.statusCode == 422) {
+        ToastUtil.showShortToast(
+          error.response?.data["message"] ?? error.message,
         );
       } else {
         ToastUtil.showShortToast(
-          error.response?.data["error"] ?? error.message,
+          error.response?.data["message"] ?? error.message,
         );
       }
     }
